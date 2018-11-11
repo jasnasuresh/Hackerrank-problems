bebeade25c55e62245526616179acd1a7e236223
jasnasuresh <jasna.suresh@gmail.com>
2018-11-11 12:58:53 +0530

    python program on extra long factorial

---
+++ extralongfact.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    f=1
+    for i in range(1,n+1):
+        f*=i
+    print(f)
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    extraLongFactorials(n)
+
